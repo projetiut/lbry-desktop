5ad2c8bbe0115b815fc326e447b3ce31615f2531
Sean Yesmunt <sean@lbry.io>
2020-12-15 14:14:51 -0500

    odysee
    update meme
    
    Use contextual "Support" string
    
    add bun icon
    
    trigger build
    
    adwords script when not embed
    
    adwords unless embed
    
    livestream initial commit
    
    cleanup
    
    fix bitwave username
    
    update livestream deets
    
    fix redirect and prevent floating player from activating when leaving the livestream page
    
    update email address and fix button spacing
    
    add live view count + continously poll bitwave to check if stream is live
    
    insert script from component rather than in html
    
    add support for countdown timer
    
    Update livestream.js
    
    fix comment
    
    remove custom ads code on odysee for more parity with master branch
    
    fix rebase
    
    update livestream info
    
    fix typo
    
    poll file reactions for 'real-time' updates
    mainly for livestreams, but will be useful everywhere
    
    emoji buttons for livestream
    
    markdown
    
    highlight streamer in comments
    
    update livestream details
    
    fetch bitwave as soon as the app finds a livestream claim
    
    use SITE_NAME for password card title
    
    update creator
    
    quick fix to add spacing on livestream page when theater mode is enabled
    
    bump lbry-redux

---
+++ ui/constants/livestream.js
@@ -0,0 +1,11 @@
+export const LIVE_STREAM_CHANNEL = '@PressForTruth';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = '49f1876b382a69414a330995908e326c7b96c132';
+export const BITWAVE_USERNAME = 'pressfortruth';
+
+// export const LIVE_STREAM_CHANNEL = '@sy-test';
+// export const LIVE_STREAM_CHANNEL_CLAIM_ID = '84b50bafd028692c90a25b83d0929defc73154b2';
+// export const BITWAVE_USERNAME = 'doomtube';
+
+export const BITWAVE_API = 'https://api.bitwave.tv/v1/channels';
+export const BITWAVE_EMBED_URL = 'https://bitwave.tv/embed';
+export const LIVE_STREAM_TAG = 'odysee-livestream';
