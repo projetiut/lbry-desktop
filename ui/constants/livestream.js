47ea933f84fca1f1c0654c28e764c96c21ffbbc2
Sean Yesmunt <sean@lbry.io>
2021-02-04 19:31:47 -0500

    update livestream.js

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,6 +1,6 @@
-export const LIVE_STREAM_CHANNEL = '@PressForTruth';
-export const LIVE_STREAM_CHANNEL_CLAIM_ID = '49f1876b382a69414a330995908e326c7b96c132';
-export const BITWAVE_USERNAME = 'pressfortruth';
+export const LIVE_STREAM_CHANNEL = '@SlavGuns';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = '6f3d20c0f872abb32701c07d5e1b667a64a29a95';
+export const BITWAVE_USERNAME = 'slavguns';
 
 // export const LIVE_STREAM_CHANNEL = '@sy-test';
 // export const LIVE_STREAM_CHANNEL_CLAIM_ID = '84b50bafd028692c90a25b83d0929defc73154b2';
